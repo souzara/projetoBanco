3a742101b8d2361674bb6318b2ec29bc1f25a569
RICARDO FRANCISCO <ricardo.francisco2@fatec.sp.gov.br>
2019-01-12 10:33:06 -0200

    Add project files.

---
+++ ProjetoBanco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
